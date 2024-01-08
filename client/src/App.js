6e2204cfd29112d8559f48abe2296457c2e59dd0
manish123k <manishksbth@gmail.com>
2024-01-09 00:07:53 +0530

    added header

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Header(){
+ return (
+  <div>
+    <ul>
+      <li><a>Home</a></li>
+      <li><a>Login</a></li>
+      <li><a>Signup</a></li>
+    </ul>
+  </div>
+ );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <h1>test....</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>test2....</h2>
-
-          Learn React :)::::456
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
