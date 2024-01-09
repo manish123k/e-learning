b3e867aa86262b1b7232f43bc5c3c1322245fdef
pramodkumar56788 <pramodkumar56788@gmail.com>
2024-01-09 13:42:37 +0530

    css add in css

--- client/src/Component/footer.js
+++ client/src/Component/footer.js
@@ -1,8 +1,8 @@
 function Footer(){
     return (
-        <footer>
-        <p>&copy; 2024 Your Website. All rights reserved. | <a href="mailto:info@example.com">pramodmanit23@gmail.com</a> | <a href="tel:+1234567890">+1 (234) 567-890</a></p>
-        <p>Follow us on: <a href="https://facebook.com/yourpage" target="_blank">Facebook</a> | <a href="https://twitter.com/yourpage" target="_blank">Twitter</a></p>
+        <footer id="footer">
+        <p>&copy; 2024 Your Website. All rights reserved. | <a href="mailto:info@example.com">pramodmanit23@gmail.com</a> | <a href="tel:+1234567890">+91 6387024144</a></p>
+        <p>Follow us on: <a href="https://www.linkedin.com/in/pramod-kumar-a8767b209/" target="_blank">Lnkedin</a> | <a href="https://www.youtube.com/channel/UCOOhxIPaBIxsWneDSdRt6sw" target="_blank">youtub</a></p>
     </footer>
     );
    
